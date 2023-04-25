b72a86c492e84e164725da2ce92ac39854ac763b
ZygimantasB <92627436+ZygimantasB@users.noreply.github.com>
2023-04-25 21:44:28 +0300

    Created models in my database and make migration

--- calories_counter/admin.py
+++ calories_counter/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Food, Meal, MealFood, UserProfile
 
 # Register your models here.
+
+
+admin.site.register(Food)
+admin.site.register(Meal)
+admin.site.register(MealFood)
+admin.site.register(UserProfile)
--- calories_counter/models.py
+++ calories_counter/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+
+class Food(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    food_name = models.CharField(max_length=255)
+    calories = models.FloatField()
+
+    def __str__(self):
+        return self.food_name
+
+
+class Meal(models.Model):
+    meal_name = models.CharField(max_length=255)
+    foods = models.ManyToManyField(Food, through="MealFood")
+
+    def __str__(self):
+        return self.meal_name
+
+
+class MealFood(models.Model):
+    meal = models.ForeignKey(Meal, on_delete=models.CASCADE)
+    food = models.ForeignKey(Food, on_delete=models.CASCADE)
+    quantity = models.FloatField()
+
+    def __str__(self):
+        return f"{self.meal} - {self.food} x {self.quantity}"
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    meals = models.ManyToManyField(Meal)
