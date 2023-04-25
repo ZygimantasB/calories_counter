5a01f6887eb84e1d7915d072bb69999779c0ebae
ZygimantasB <92627436+ZygimantasB@users.noreply.github.com>
2023-04-25 21:51:07 +0300

    Fix few mistakes with files witch dont need

--- calories_counter/apps.py
+++ calories_counter/apps.py
@@ -5,5 +5,3 @@ class CaloriesCounterConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "calories_counter"
 
-    def ready(self):
-        import calories_counter.signals
