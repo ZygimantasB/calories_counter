a99c172ed66b0d1293124c709ae5a911a7441eb6
ZygimantasB <92627436+ZygimantasB@users.noreply.github.com>
2023-04-25 21:19:15 +0300

    To hard save but this part are abandend

---
+++ calories_counter/forms.py
@@ -0,0 +1,27 @@
+from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.models import User
+from .models import Food, Profile
+
+
+class SelectFoodForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('food_selected', 'quantity',)
+
+    def __init__(self, user, *args, **kwargs):
+        super(SelectFoodForm, self).__init__(*args, **kwargs)
+        self.fields['food_selected'].queryset = Food.objects.filter(person_of=user)
+
+
+class AddFoodForm(forms.ModelForm):
+    class Meta:
+        model = Food
+        fields = ('name', 'quantity', 'calorie')
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('calorie_goal',)
--- calories_counter/models.py
+++ calories_counter/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
+
+
+class Food(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=False)
+    quantity = models.PositiveIntegerField(null=False, default=0)
+    calorie = models.FloatField(null=False, default=0)
+
+    def __str__(self):
+        return self.name
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    calories_count = models.FloatField(null=False, default=0, blank=True)
+    food_selected = models.ForeignKey(Food, on_delete=models.CASCADE, null=True, blank=True)
+    quantity = models.FloatField(default=0)
+    total_calories = models.FloatField(default=0)
+    date = models.DateField(auto_now_add=True)
+    calories_goal = models.PositiveIntegerField(default=0)
+    all_food_selected_today = models.ManyToManyField(Food, through='PostFood', related_name="inventory", blank=True)
+
+    def save(self, *args, **kwargs):
+        if self.food_selected != None:
+            self.amount = (self.food_selected.calorie/self.food_selected.quantity)
+            self.calorie_count = self.amount*self.quantity
+            self.total_calorie = self.calorie_count + self.total_calories
+            calories = Profile.objects.filter(person_of=self.user).last()
+            PostFood.objects.create(profile=calories,food=self.food_selected,calorie_amount=self.calorie_count,amount=self.quantity)
+            self.food_selected = None
+            super(Profile, self).save(*args,**kwargs)
+
+        else:
+            super(Profile,self).save(*args,**kwargs)
+
+    def __str__(self):
+        return self.user
+
+
+class PostFood(models.Model):
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    food = models.ForeignKey(Food,on_delete=models.CASCADE)
+    calorie_amount = models.FloatField(default=0,null=True,blank=True)
+    amount = models.FloatField(default=0)
