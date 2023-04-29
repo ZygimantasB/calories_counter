84d7c28cc21c16bddb82eeb61694a8931dc1934a
ZygimantasB <92627436+ZygimantasB@users.noreply.github.com>
2023-04-29 18:48:51 +0300

    BLog has working models and admin

--- calories_blog/admin.py
+++ calories_blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Post, Author, Tag
 
 # Register your models here.
+
+admin.site.register(Post)
+admin.site.register(Author)
+admin.site.register(Tag)
--- calories_blog/models.py
+++ calories_blog/models.py
@@ -1,3 +1,37 @@
+from django.core.validators import MinLengthValidator
 from django.db import models
 
 # Create your models here.
+
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.caption
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email_address = models.EmailField()
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    excerpt = models.CharField(max_length=250)
+    image_name = models.CharField(max_length=100)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True, db_index=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name="posts")
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
+
+
+
