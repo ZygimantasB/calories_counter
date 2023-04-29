4c0c39508ca716a0e2c6941b54538e37ce14a0ac
ZygimantasB <92627436+ZygimantasB@users.noreply.github.com>
2023-04-29 16:20:59 +0300

    Fixing all posts

--- calories_blog/urls.py
+++ calories_blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("calories_blog/", views.start_page, name="start_page"),
+    path("calories_blog/", views.start_page_cal, name="start_page_cal"),
     path("calories_blog/posts/", views.posts, name="posts"),
     path("calories_blog/posts/<slug:slug>/", views.single_post, name="single_post"),
     ]
--- calories_blog/views.py
+++ calories_blog/views.py
@@ -3,12 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def start_page(request):
-    return render(request, "calories_blog/start_page.html")
+def start_page_cal(request):
+    return render(request, "calories_blog/start_page_cal.html")
 
 
 def posts(request):
-    pass
+    return render(request, "calories_blog/all-posts.html")
 
 
 def single_post(request, slug):
