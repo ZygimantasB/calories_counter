46958f9a67ef7d60f11fef293a568f0e55e53393
ZygimantasB <92627436+ZygimantasB@users.noreply.github.com>
2023-04-30 19:16:14 +0300

    I added few post changed pictures

--- calories_blog/views.py
+++ calories_blog/views.py
@@ -29,76 +29,81 @@ class AllPostsView(ListView):
     context_object_name = "all_posts"
 
 
-class SinglePostView(View):  # slug supported by default
-    # template_name = "calories_blog/post-detail.html"
-    # model = Post
+class SinglePostView(View):
+    def is_stored_post(self, request, post_id):
+        stored_posts = request.session.get('stored_posts')
+        if stored_posts is not None:
+            is_saved_for_later = post_id in stored_posts
+        else:
+            is_saved_for_later = False
+
+        return is_saved_for_later
+
     def get(self, request, slug):
         post = Post.objects.get(slug=slug)
-        content = {
-                    "post": post,
-                    "post_tags": post.tags.all(),
-                    "comment_form": CommentForm(),
-                    "comments": post.comments.all().order_by("-id"),
-                   }
-        return render(request, "calories_blog/post-detail.html", content)
+
+        context = {
+            'post': post,
+            'post_tags': post.tags.all(),
+            'comment_form': CommentForm,
+            'comments': post.comments.all().order_by('-id'),
+            'saved_for_later': self.is_stored_post(request, post.id)
+        }
+        return render(request, "calories_blog/post-detail.html", context)
 
     def post(self, request, slug):
         comment_form = CommentForm(request.POST)
         post = Post.objects.get(slug=slug)
-
         if comment_form.is_valid():
-            comment = comment_form.save(commit=False)  # create object but not save to db
+            comment = comment_form.save(commit=False)
             comment.post = post
             comment.save()
 
-            return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
+            return HttpResponseRedirect(reverse('post-detail-page', args=[slug]))
 
-        content = {
-                "post": post,
-                "post_tags": post.tags.all(),
-                "comment_form": CommentForm(),
-                "comments": post.comments.all().order_by("-id"),
-               }
+        context = {
+            'post': post,
+            'post_tags': post.tags.all(),
+            'comment_form': comment_form,
+            'comments': post.comments.all(),
+            'saved_for_later': self.is_stored_post(request, post.id),
+        }
 
-        return render(request, "calories_blog/post-detail.html", content)
-
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context["post_tags"] = self.object.tags.all()
-    #     context["comment_form"] = CommentForm()
-    #     return context
+        return render(request, "calories_blog/post-detail.html", context)
 
 
 class ReadLaterView(View):
-    def post(self, request):
-        stored_post = request.session.get("stored_post")
-
-        if stored_post is None:
-            stored_post = []
-
-        post_id = int(request.POST["post_id"])
-
-        if post_id not in stored_post:
-            stored_post.append(post_id)
-
-        request.session["stored_post"] = stored_post
-
-        return HttpResponseRedirect("/")
-
     def get(self, request):
-        stored_post = request.session.get("stored_post")
+        stored_posts = request.session.get('stored_posts')
 
         context = {}
 
-        if stored_post is None or len(stored_post) == 0:
-            context["posts"] = []
-            context["has_posts"] = False
+        if stored_posts is None or len(stored_posts) == 0:
+            context['posts'] = None
+            context['has_posts'] = False
         else:
-            posts = Post.objects.filter(id__in=stored_post)
-            context["posts"] = posts
-            context["has_posts"] = True
+            posts = Post.objects.filter(id__in=stored_posts)
+            context['posts'] = posts
+            context['has_posts'] = True
 
         return render(request, "calories_blog/stored-posts.html", context)
+
+    def post(self, request):
+        stored_posts = request.session.get('stored_posts')
+
+        if stored_posts is None:
+            stored_posts = []
+
+        post_id = int(request.POST['post_id'])
+
+        if post_id not in stored_posts:
+            stored_posts.append(post_id)
+            request.session['stored_posts'] = stored_posts
+        else:
+            stored_posts.remove(post_id)
+            request.session['stored_posts'] = stored_posts
+
+        return HttpResponseRedirect('/')
 
 
 
